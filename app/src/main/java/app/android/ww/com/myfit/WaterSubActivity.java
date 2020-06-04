09248f7cc258895ad2cb06f2eb4a8aa9c496526c
kiria <kiria77@naver.com>
2020-06-04 23:30:17 +0900

    2020.06.04

---
+++ app/src/main/java/app/android/ww/com/myfit/WaterSubActivity.java
@@ -0,0 +1,13 @@
+package app.android.ww.com.myfit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class WaterSubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_sub);
+    }
+}
